dacf1dddb5593edd120cf47b3821e5b2b49301cb
Abdullah Juma <Abdullah.Juma280@hotmail.com>
2026-03-29 15:34:18 +0400

    testing new work for file

---
+++ src/Javatest.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArraylistJava {
+    public static void main(String[] args) {
+
+
+        Arraylist <String> car = new ArrayList<>();
+
+        
+    }
+}
