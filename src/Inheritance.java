50b90678bc799178d0a2429ecc7b4e4aec3ae275
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-04-13 09:03:20 +0400

    Inheritance new task :)

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,24 +1,29 @@
-public class Inheritance {
 
-    // father class
-    class Animal {
-        void eat() {
-            System.out.println("eating");
+    class Student{
+        private String name;
+        private double grade;
+
+        public Student(String name,double grade){
+            this.name=name;
+            this.grade=grade;
         }
+
+        public void getInfo(){
+            System.out.println("Name: "+name);
+            System.out.println("Grade: "+grade);
+        }
+
+
     }
 
-    // son class
-    class Dog extends Animal {
-        void bark() {
-            System.out.println("barking");
-        }
-    }
-
+    public class Inheritance { // file name
     public static void main(String[] args) {
-        Inheritance i = new Inheritance();
-        Dog d = i.new Dog();
-        d.eat(); // Inherited method
-        d.bark(); // Dog's own method
+        Inheritance i=new Inheritance();
+        Student s1 = new Student("Alice", 85.5);
+        Student s2 = new Student("Bob", 90.5);
+        s1.getInfo();
+        s2.getInfo();
+
     }
 
 }
