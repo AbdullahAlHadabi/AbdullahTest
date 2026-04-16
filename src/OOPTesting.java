0e5c6f645d3d049bd4bdc8ac247595de66ce30a0
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-04-16 09:04:17 +0400

    adding search for the student

--- src/OOPTesting.java
+++ src/OOPTesting.java
@@ -9,10 +9,12 @@ public class OOPTesting {
         Scanner input = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n1. Add Student");
+            System.out.println("1. Add Student");
             System.out.println("2. Show Students");
             System.out.println("3. Top Student");
-            System.out.println("4. Exit");
+            System.out.println("4. Lowest Student");
+            System.out.println("5. Search Student");
+            System.out.println("6. Exit");
 
             int choice = input.nextInt();
             input.nextLine();
@@ -24,6 +26,7 @@ public class OOPTesting {
                 System.out.print("Enter score: ");
                 int score = input.nextInt();
                 input.nextLine();
+
 
                 Student s = new Student();
                 s.name = name;
@@ -37,7 +40,7 @@ public class OOPTesting {
                 }
 
             } else if (choice == 3) {
-                if (students.size() > 0) {
+                if (!students.isEmpty()) {
                     Student top = students.get(0); //For now I will assume the FIRST student is the best
 
                     for (Student s : students) {
@@ -46,11 +49,11 @@ public class OOPTesting {
                         }
                     }
 
-                    System.out.println("Top student: " + top.name);
+                    System.out.println("Top student: " + top.name + " --- The score :  " + top.score);
                 }
             } else {
                 if (choice == 4) {
-                    if (students.size() > 0) {
+                    if (!students.isEmpty()) {
                         Student lowest = students.get(0);
                         for (Student s : students) {
                             if (s.score < lowest.score) {
@@ -59,12 +62,38 @@ public class OOPTesting {
                         }
                         System.out.println("Lowest student: " + lowest.name + " - " + lowest.score);
                     }
-                    ;
+
+                } else if (choice == 5) {
+
+                    System.out.print("Enter name to search: ");
+                    String searchName = input.nextLine();
+
+                    boolean found = false;
+
+                    for (Student s : students) {
+                        if (s.name.equals(searchName)) {
+                            System.out.println("Found: " + s.name + " - " + s.score);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Student not found");
+                    }
                 }
+                if (choice == 6) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+
             }
         }
+
     }
 }
+
 
 
 class Student {
